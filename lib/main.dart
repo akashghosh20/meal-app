47f0defb2f11192a2bc71619fd3e3f711c58a3dc
Akash Ghosh <ag1229222@gmail,com>
2024-09-01 20:36:27 +0600

    a

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Meal App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
